4ce4f3e210ee5bf75c47ad6ed281ec7b3a597604
Jeff Rock <SunShineOneX@gmail.com>
2020-07-29 10:10:47 -0400

    npm installs and navbar upd

---
+++ src/components/Navbar.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+import { Routerbrowser as Router, Link, Switch, Route } from "react-router-dom";
+import axios from "axios";
+
+
+ class Navbar extends Component {
+    render() {
+        return (
+            <div>
+                <h1> Hello world! </h1>
+            </div>
+        )
+    }
+}
+
+export default Navbar;
