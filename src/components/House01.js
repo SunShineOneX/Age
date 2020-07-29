8e845d4e525b15b93b9814d68fe7c7fc727eb03a
Jeff Rock <SunShineOneX@gmail.com>
2020-07-29 11:11:49 -0400

    house updates

--- src/components/House01.js
+++ src/components/House01.js
@@ -1,4 +1,7 @@
 import React, { Component } from 'react'
+import { Routerbrowser as Router, Link, Switch, Route } from "react-router-dom";
+import axios from "axios";
+
 
 class House01 extends Component {
     render() {
