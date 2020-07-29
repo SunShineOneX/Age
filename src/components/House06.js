75d5216f286883793c7ccb13eed1610a37979e2b
Jeff Rock <SunShineOneX@gmail.com>
2020-07-29 11:52:38 -0400

    house05 upd

--- src/components/House06.js
+++ src/components/House06.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { Routerbrowser as Router, Link, Switch, Route } from "react-router-dom";
+import { BrowserRouter as Router, Link, Switch, Route } from "react-router-dom";
 import axios from "axios";
 
 
