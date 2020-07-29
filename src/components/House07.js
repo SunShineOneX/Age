26c8cea9ffad6020929cfdc56e5ca7a0f6e2abcc
Jeff Rock <SunShineOneX@gmail.com>
2020-07-29 11:13:00 -0400

    house 07 upd

---
+++ src/components/House07.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+import { Routerbrowser as Router, Link, Switch, Route } from "react-router-dom";
+import axios from "axios";
+
+
+class House01 extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Hello House Stark!</h1>
+            </div>
+        )
+    }
+}
+
+
+export default House01
