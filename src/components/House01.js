6cb14a4abd86c4b498ab66d0f0d4b9a240af22ec
Jeff Rock <SunShineOneX@gmail.com>
2020-07-30 10:33:53 -0400

    house stark updates complete for now besides additional styles

--- src/components/House01.js
+++ src/components/House01.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Link, Switch, Route } from "react-router-dom";
 import axios from "axios";
+import "../stark.css"
 const API_CALL = "https://www.anapioficeandfire.com/api/houses/362";
 
 class House01 extends Component {
@@ -38,10 +39,10 @@ class House01 extends Component {
         <h1>{this.state.info.name}</h1>
         <h2>"{this.state.info.words}"</h2>
         <p>House Stark of Winterfell is a Great House of Westeros and the royal house of the Kingdom of the North. They rule over the vast region known as the North from their seat in Winterfell. It is one of the oldest lines of Westerosi nobility by far, claiming a line of descent stretching back over eight thousand years. Before the Targaryen conquest, as well as during the War of the Five Kings and early on in Daenerys Targaryen's war for Westeros, the leaders of House Stark ruled over the region as the Kings in the North.</p>
-        <h2>Region:</h2>
-        <h3>{this.state.info.region}</h3>
+        <h2>Region: {this.state.info.region}</h2>
         <h4>Cadet houses {this.state.cadetBranch1}</h4>
         <h4>{this.state.cadetBranch2}</h4>
+        <h5>Founded during the {this.state.info.founded}</h5>
         
       </div>
     );
