d94826010c426baf4c601ad3244675da7fa54889
Jeff Rock <SunShineOneX@gmail.com>
2020-07-30 10:46:33 -0400

    house targaryen async calls working

--- src/components/House02.js
+++ src/components/House02.js
@@ -1,14 +1,34 @@
 import React, { Component } from 'react'
 import { BrowserRouter as Router, Link, Switch, Route } from "react-router-dom";
 import axios from "axios";
-
-
+import "../targaryen.css";
+const api_call = "https://www.anapioficeandfire.com/api/houses/378";
 
 class House02 extends Component {
+    constructor() {
+        super();
+        this.state = {
+            info: [],
+        };
+    }
+
+    async getHouseInfo() {
+        try {
+            const res = await axios.get(api_call);
+            this.setState({ info: res.data})
+        
+    } catch {
+        console.log("error");
+    }
+}
+    componentDidMount() {
+        this.getHouseInfo();
+    }
+
     render() {
         return (
             <div>
-                <h1>Hello House Targaryen!</h1>
+                <h1>{this.state.info.name}</h1>
             </div>
         )
     }
