ca5b6a2330609c3fbe830d617ae579d968a01c9c
Jeff Rock <SunShineOneX@gmail.com>
2020-07-30 10:00:28 -0400

    styles and info upd for house 01

--- src/components/House01.js
+++ src/components/House01.js
@@ -34,8 +34,11 @@ class House01 extends Component {
   render() {
     return (
       <div>
+          <img className = "stark-logo" src={require("../assets/stark01.jpg")} />
         <h1>{this.state.info.name}</h1>
         <h2>"{this.state.info.words}"</h2>
+        <p>House Stark of Winterfell is a Great House of Westeros and the royal house of the Kingdom of the North. They rule over the vast region known as the North from their seat in Winterfell. It is one of the oldest lines of Westerosi nobility by far, claiming a line of descent stretching back over eight thousand years. Before the Targaryen conquest, as well as during the War of the Five Kings and early on in Daenerys Targaryen's war for Westeros, the leaders of House Stark ruled over the region as the Kings in the North.</p>
+        <h2>Region:</h2>
         <h3>{this.state.info.region}</h3>
         <h4>Cadet houses {this.state.cadetBranch1}</h4>
         <h4>{this.state.cadetBranch2}</h4>
