c8cfcc63747512bc08b20ab2a26e89d369dfa51b
Jeff Rock <SunShineOneX@gmail.com>
2020-07-30 17:58:44 -0400

    house baratheon axios calls working

--- src/components/House06.js
+++ src/components/House06.js
@@ -2,12 +2,33 @@ import React, { Component } from 'react'
 import { BrowserRouter as Router, Link, Switch, Route } from "react-router-dom";
 import axios from "axios";
 
+const base_url = "https://www.anapioficeandfire.com/api/houses/17";
 
 class House06 extends Component {
+    constructor() {
+        super();
+        this.state = {
+            info: [],
+        }
+    }
+
+    async getHouseInfo() {
+        try {
+            const res = await axios.get(base_url);
+            this.setState({ info: res.data})
+        } catch {
+            console.log("error")
+        }
+    }
+
+    componentDidMount() {
+        this.getHouseInfo();
+    }
+
     render() {
         return (
             <div>
-                <h1>Hello House Baratheon!</h1>
+                <h1>{this.state.info.name}</h1>
             </div>
         )
     }
